db5252eefa877f424a16a9014b2a1669ac73d1a2
mac <2412235462@qq.com>
2022-10-18 20:33:15 +0800

    vite.config.ts修改server代理配置解决跨域问题

--- src/utils/http/request.ts
+++ src/utils/http/request.ts
@@ -4,7 +4,7 @@
  * @Author: Chenyx
  * @Date: 2022-10-16 21:23:00
  * @LastEditors: Chenyx
- * @LastEditTime: 2022-10-18 01:11:48
+ * @LastEditTime: 2022-10-18 03:36:40
  */
 import axios from "axios"
 import { error } from "console"
@@ -21,7 +21,7 @@ const service = axios.create({
 
 service.interceptors.request.use(
     config => {
-        config.baseURL = "/api"
+        // config.baseURL = "/api"
         return config
     },
     error => {
--- vite.config.ts
+++ vite.config.ts
@@ -10,11 +10,11 @@ const srcPath = path.resolve(__dirname, "src")
 // https://vitejs.dev/config/
 export default defineConfig({
   server:{
-    port: 8888,
+    port: 8088,
     open: true,
     proxy:{
       '/api':{
-        target:"http://localhost:8888",
+        target:"http://127.0.0.1:8088",
         changeOrigin:true,
         rewrite:(path)=>path.replace(/^\/api/,'')
       }
