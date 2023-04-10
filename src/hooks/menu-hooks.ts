946a32d0afc1312a44d49a3a56d45892add769e1
mac <2412235462@qq.com>
2023-04-11 00:54:36 +0800

    test:test

--- src/hooks/menu-hooks.ts
+++ src/hooks/menu-hooks.ts
@@ -2,7 +2,7 @@
  * @Author: chenyx
  * @Date: 2023-04-01 02:15:34
  * @LastEditors: Do not edit
- * @LastEditTime: 2023-04-11 00:52:58
+ * @LastEditTime: 2023-04-11 00:54:00
  * @FilePath: /chenyxjs-blog/src/hooks/menu-hooks.ts
  */
 import router from "@/router";
@@ -10,8 +10,8 @@ import { ElMessage } from "element-plus";
 
 export function useMenu() {
   function toPortal() {
-    ElMessage.warning("模块正在调试中，敬请期待...");
-     router.push("/static");
+    // ElMessage.warning("模块正在调试中，敬请期待...");
+    router.push("/static");
   }
   function toHome() {
     router.push("/home");
