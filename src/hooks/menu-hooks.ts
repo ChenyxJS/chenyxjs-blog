602e84a97bf8b9a48a7a5ded3b2c211cc9e0c364
mac <2412235462@qq.com>
2023-04-11 01:19:38 +0800

    build:打包

--- src/hooks/menu-hooks.ts
+++ src/hooks/menu-hooks.ts
@@ -2,7 +2,7 @@
  * @Author: chenyx
  * @Date: 2023-04-01 02:15:34
  * @LastEditors: Do not edit
- * @LastEditTime: 2023-04-11 01:08:48
+ * @LastEditTime: 2023-04-11 01:17:23
  * @FilePath: /chenyxjs-blog/src/hooks/menu-hooks.ts
  */
 import router from "@/router";
@@ -11,7 +11,7 @@ import { ElMessage } from "element-plus";
 export function useMenu() {
   function toPortal() {
     // ElMessage.warning("模块正在调试中，敬请期待...");
-    router.push("/static/index.html");
+    window.open('http://www.chenyx.site/static/index.html','_blank')
   }
   function toHome() {
     router.push("/home");
