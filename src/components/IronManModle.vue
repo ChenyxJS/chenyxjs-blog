08c7793695e8a3a232d88eb4c43bda3b4bfc530a
Chenyx <2412235462@qq.com>
2023-08-21 01:45:10 +0800

    feat: 首页钢铁侠模型

--- src/components/IronManModle.vue
+++ src/components/IronManModle.vue
@@ -4,10 +4,13 @@ import {
     PerspectiveCamera,
     WebGLRenderer,
     DirectionalLight,
+    HemisphereLight,
+    SpotLight,
 } from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { onMounted, ref } from "vue";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
 
 // 拿到渲染dom节点的实例
 const threeRef = ref();
@@ -24,13 +27,23 @@ const camera = new PerspectiveCamera(
 );
 // 模型加载器
 const loader = new GLTFLoader();
+const dracoLoader = new DRACOLoader();
+// 设置解压库文件路径
+dracoLoader.setDecoderPath("/draco/gltf/");
+// 使用js方式解压
+dracoLoader.setDecoderConfig({ type: "js" });
+// 初始化_initDecoder 解码器
+dracoLoader.preload();
+// gltfloader使用dracoLoader
+loader.setDRACOLoader(dracoLoader);
+
 // 轨道控制器
 const controls = new OrbitControls(camera, renderer.domElement);
 
 onMounted(() => {
     // 初始化场景、模型
     init();
-    change();
+    resize();
 });
 
 function init() {
@@ -41,14 +54,20 @@ function init() {
     loop();
 }
 
-function change() {
+function resize() {
     window.addEventListener("resize", () => setCamera());
 }
 
 function setControls() {
     // 控制器
     controls.maxPolarAngle = (0.9 * Math.PI) / 2;
+    // 开启缩放
     controls.enableZoom = true;
+    // 限制缩放倍数
+    controls.minDistance = 3;
+    controls.maxDistance = 5;
+    // 自动旋转
+    controls.autoRotate = true;
 }
 
 function setCamera() {
@@ -62,13 +81,22 @@ function setCamera() {
 }
 
 function loadIronManFile() {
+    const url = "/iron_man_mark_85.glb";
     loader.load(
-        "src/assets/3d/iron_man_mark_85/scene.gltf",
+        url,
         (gltf: any) => {
             const model = gltf.scene;
             model.position.set(0, -0.5, 0); // 模型位置
             model.scale.set(3, 1, 3);
             scene.add(gltf.scene); // 加入场景
+
+            model.traverse((child: any) => {
+                if (child.isMesh) {
+                    //如果是物体就输出名字
+                    console.log(child.name);
+                    // console.log(child)
+                }
+            });
         },
         ({ loaded, total }) => {
             let load = Math.abs((loaded / total) * 100);
@@ -94,7 +122,24 @@ function setLight() {
     light7.position.set(50, 0, 0);
     let light8 = new DirectionalLight(0xffffff, 1);
     light8.position.set(-50, 0, 0);
-    scene.add(light1, light2, light3, light4, light5, light6, light7, light8);
+    let light9 = new DirectionalLight(0xffffff, 1);
+    light9.position.set(0, 0, 0);
+    scene.add(
+        light1,
+        light2,
+        light3,
+        light4,
+        light5,
+        light6,
+        light7,
+        light8,
+        light9
+    );
+    let hemisphereLight = new HemisphereLight(0xff0000, 0x0000ff);
+    scene.add(hemisphereLight);
+    let spotLight = new SpotLight(0xffffff);
+    spotLight.position.set(100, 1000, 100);
+    scene.add(spotLight);
 }
 
 function loop() {
